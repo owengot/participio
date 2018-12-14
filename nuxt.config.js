3f60a33d1db2ca087fcf4ed7eeaecdc3cb459e15
Owen <owengot@gmail.com>
2018-12-14 20:55:47 +0200

    v0.15

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 module.exports = {
   head: {
-    titleTemplate: '%s - Blog',
+    titleTemplate: 'Particip_IO | Edgeryders',
     meta: [{
         charset: 'utf8'
       },
@@ -26,9 +26,10 @@ module.exports = {
       href: 'https://fonts.googleapis.com/css?family=Exo+2:100,200,300,400,500|Open+Sans:100,200,400,600,700'
     }]
   },
-  css: [
-    { src: '@/assets/global.sass', lang: 'sass' },
-  ],
+  css: [{
+    src: '@/assets/global.sass',
+    lang: 'sass'
+  }, ],
   router: {
     base: '/'
     // base: '/simple-blog-with-nuxt/'
